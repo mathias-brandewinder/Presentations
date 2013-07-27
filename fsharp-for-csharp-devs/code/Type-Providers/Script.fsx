7cdb94a0730ad18f45ae4c985f8723f128c5c187
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2013-07-27 08:57:41 -0700

    Added Type Provider demo

--- fsharp-for-csharp-devs/code/Type-Providers/Script.fsx
+++ fsharp-for-csharp-devs/code/Type-Providers/Script.fsx
@@ -1,8 +1,52 @@
-﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
-// for more guidance on F# programming.
+﻿#r @"..\packages\FSharp.Data.1.1.9\lib\net40\FSharp.Data.dll"
+#r @"..\packages\RProvider.1.0.0\lib\RDotNet.dll"
+#r @"..\packages\RProvider.1.0.0\lib\RProvider.dll"
 
-#load "Library1.fs"
-open Type_Providers
+open System
+open FSharp.Data
+open RDotNet
+open RProvider
+open RProvider.``base``
+open RProvider.graphics
 
-// Define your library scripting code here
+// we get access to live data from the World Bank,
+// with IntelliSense for discoverability
+let wb = WorldBankData.GetDataContext()
+let capital = wb.Countries.Afghanistan.CapitalCity
 
+// We can now work against R, with Intellisense...
+let rng = Random()
+let data = R.c([| for i in 0 .. 1000 -> rng.NextDouble() |])
+R.plot(data)
+R.hist(data |> R.log)
+
+// ... and grab live data from the World Bank,
+// and send it to R for visualization
+let countries = [|
+    wb.Countries.Canada;
+    wb.Countries.``United States``;
+    wb.Countries.Mexico;
+    wb.Countries.Brazil;
+    wb.Countries.Argentina;
+    wb.Countries.``United Kingdom``;
+    wb.Countries.France;
+    wb.Countries.Germany;
+    wb.Countries.``South Africa``;
+    wb.Countries.Kenya;
+    wb.Countries.``Russian Federation``;
+    wb.Countries.China;
+    wb.Countries.Japan;
+    wb.Countries.Australia |]
+
+let gdp2000 = countries |> Array.map (fun c -> c.Indicators.``GDP (current US$)``.[2000])
+let gdp2010 = countries |> Array.map (fun c -> c.Indicators.``GDP (current US$)``.[2010])
+
+let series = [
+    "GDP2000", gdp2000;
+    "GDP2010", gdp2010; ]
+
+let dataframe = R.data_frame(namedParams series)
+R.plot(dataframe)
+
+let names = countries |> Array.map (fun c -> c.Name)
+R.text(gdp2000, gdp2010, names)
