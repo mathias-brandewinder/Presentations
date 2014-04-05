bda3e497c321aedf21235c51c302a3201a53d397
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2014-04-04 21:47:42 -0700

    Added test runner

---
+++ fsharp-for-csharp-devs/code/CSharp-Beginning/TestsRunner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace CSharp_Beginning
+{
+    [TestFixture]
+    public class TestsRunner
+    {
+        [Test]
+        public void RunCode()
+        {
+            
+        }
+    }
+}
